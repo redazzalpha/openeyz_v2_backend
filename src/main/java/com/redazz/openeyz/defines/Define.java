3d2ec374f517cda81ee49c953d1388628fd41f57
kyzer <redazzkillah@gmail.com>
2023-01-26 16:42:09 +0100

    modify content upload size to 40mb

--- src/main/java/com/redazz/openeyz/defines/Define.java
+++ src/main/java/com/redazz/openeyz/defines/Define.java
@@ -68,6 +68,5 @@ public class Define {
     
     public static final int REGISTER_NUM_ARG = 4;
 
-    public static final int MULTIPART_UPLOAD_SIZE = 10;
-//    public static final int MULTIPART_UPLOAD_SIZE = 20000000;
+    public static final int MULTIPART_UPLOAD_SIZE = 40000000;
 }
